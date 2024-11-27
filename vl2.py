1fdac2a4ff755a4d11bcbe37bef5c7b97e164910
Le Duc Minh <minhti09092006@gmail.com>
2024-11-27 11:17:39 +0700

    finishing touches

--- vl2.py
+++ vl2.py
@@ -3,79 +3,75 @@ import numpy as np
 import sympy as sp
 import matplotlib.pyplot as plt
 #Khai biến
-x = sp.symbols("x")
-y = sp.symbols("y")
-t = sp.symbols("t")
-v0 = sp.symbols("v_0")
-alpha1 = sp.symbols("alpha_1")
-alpha2 = sp.symbols("alpha_2")
-g = sp.symbols("g") #Gia tốc trọng trường
-#Giải phương trình và trả về kết quả khác một số được chọn c
-def solve(a,b,c):
-    l = sp.solve(a,b)
-    return l[0] if round(float(l[0]),2) != round(float(c),2) else l[1]
+x, y, t, v0, alpha1, alpha2, g = sp.symbols("x y t v_0 alpha_1 alpha_2 g")
+
+#Giải phương trình và trả về kết quả khác một số được chọn 
+def solve(eq, var, excluded_sol):
+    solutions = sp.solve(eq,var)
+    return solutions[0] if round(float(solutions[0]), 2) != round(float(excluded_sol), 2) else solutions[1]
+
 #Vận tốc ban đầu và gia tốc của hai phương
 vx0 = v0*sp.cos(alpha1)
 vy0 = v0*sp.sin(alpha1)
 ax = 0
 ay = -g
 
-#Vận tốc của phương x và y theo thời gian
+#Vận tốc và tọa độ của phương x và y theo thời gian
 vy_t = vy0 + sp.integrate(ay,t)
 vx_t = vx0 + sp.integrate(ax,t)
-
-#Tọa độ của phương x và y theo thời gian
 x_t = sp.integrate(vx_t,t)
 y_t = sp.integrate(vy_t,t)
 
 #Tìm thời điểm chạm đất
-T = solve(y_t,t,0) #Giải y_t = 0 và tìm t lúc đó    
+T = solve(y_t, t, 0) #Giải y_t = 0 và tìm t lúc đó    
 
 #Quãng đường theo phương x đi được, gọi khoảng cách này là L1
-L1 = sp.simplify(x_t.subs(t,T))
+L1 = sp.simplify(x_t.subs(t, T))
 #Tương tự, ta có khoảng cách L2 của góc ném 2 là khi thế alpha1 với alpha2
-L2 = L1.subs(alpha1,alpha2)
+L2 = L1.subs(alpha1, alpha2)
 
 #Đặt L1 = L2 và tìm alpha2 theo alpha1
-alpha2eq = sp.solve(L1-L2,alpha2)[0]
+alpha2eq = sp.solve(L1-L2, alpha2)
 #Giải L1 - L2 = 0 và tìm biểu thức alpha2 theo alpha1
 
-#input
+#Nhập input
 while True:
-    inp1 = float(input("Nhập alpha1: "))
-    if not inp1>np.pi or inp1<0:#Bắt trường hợp lỗi
+    alpha1_input = float(input("Nhập alpha1: "))
+    if not 0 > alpha1_input > np.pi:#Bắt trường hợp lỗi
         break
     print("Góc không hợp lệ, vui lòng nhập lại.")
-inp2 = float(input("Nhập v0: "))
+v0_input = float(input("Nhập v0: "))
 
 #Tính giá trị của alpha2
-L1num=L1.subs(alpha1,inp1)    
-a2 = solve(L1num-L2,alpha2,inp1) if inp1 < np.pi/2 else np.pi+solve(L1num-L2,alpha2,inp1)#Bắt trường hợp góc lớn hơn pi/2
+#Thế số tìm giá trị của L1
+L1num=L1.subs(alpha1, alpha1_input)
+a2 = (solve(L1num - L2, alpha2, alpha1_input) if alpha1_input < np.pi/2
+      else np.pi+solve(L1num - L2, alpha2, alpha1_input))#Bắt trường hợp góc lớn hơn pi/2
 
 #print phương trình và kết quả:
-print(f" Phương trình của alpha_2 theo alpha_1 là: \n {alpha2eq}\n Kết quả số là: {a2}")
+print(f" Phương trình của alpha_2 theo alpha_1 là: \n {alpha2eq[0]} khi alpha1 < pi/4 và {alpha2eq[1]} khi alpha1 > pi/4 \n Kết quả số là: {a2}")
 
 #Thế các biến. T1, T2 lần lượt là thời gian chạm đất của 2 quỹ đạo
-T1 = float(T.subs([(g,9.81), (v0, inp2), (alpha1,inp1)]))
-T2 = float(T.subs([(g,9.81), (v0, inp2), (alpha1,a2)]))
+T1 = float(T.subs([(g, 9.81), (v0, v0_input), (alpha1, alpha1_input)]))
+T2 = float(T.subs([(g, 9.81), (v0, v0_input), (alpha1, a2)]))
 
 #Thế các biến cho 2 hàm
-y_t1 = y_t.subs([(g,9.81), (v0,inp2), (alpha1,inp1)])
-x_t1 = x_t.subs([(g,9.81), (v0,inp2), (alpha1, inp1)])
-y_t2 = y_t.subs([(g,9.81), (v0,inp2), (alpha1, a2)])
-x_t2 = x_t.subs([(g,9.81), (v0,inp2), (alpha1, a2)])
+y_t1 = y_t.subs([(g, 9.81), (v0, v0_input), (alpha1, alpha1_input)])
+x_t1 = x_t.subs([(g, 9.81), (v0, v0_input), (alpha1, alpha1_input)])
+y_t2 = y_t.subs([(g, 9.81), (v0, v0_input), (alpha1, a2)])
+x_t2 = x_t.subs([(g, 9.81), (v0, v0_input), (alpha1, a2)])
+
 #Chuyển các hàm sympy về các hàm numpy có thể thế số
 x1 = sp.lambdify(t, x_t1, 'numpy')
 y1 = sp.lambdify(t, y_t1, 'numpy')
-x2 = sp.lambdify(t,x_t2, 'numpy')
+x2 = sp.lambdify(t, x_t2, 'numpy')
 y2 = sp.lambdify(t, y_t2, 'numpy')
 
 # Plot hai hàm
-t1 = np.linspace(0,T1,100)
-t2 = np.linspace(0,T2,100)
+t1 = np.linspace(0, T1, 100)
+t2 = np.linspace(0, T2, 100)
 plt.plot(x1(t1), y1(t1), label='Góc ném 1')
 plt.plot(x2(t2), y2(t2), label='Góc ném 2')
-
 plt.xlabel('x(t)')
 plt.ylabel('y(t)')
 plt.title('Hai quỹ đạo tương ứng với hai góc ném')
